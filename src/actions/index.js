0a145b091ebfb05f3247030a516f57141f74418e
Nick Phillips <nicktgphillips@gmail.com>
2016-11-24 20:11:32 +0000

    created atoz menu fetch data when links are clicked

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const GET_PROGRAMMES = "GET_PROGRAMMES";
 const BASE_URI = "https://ibl.api.bbci.co.uk"
 
 export function getProgrammes(letter, page) {
-  const request = axios.get(`${BASE_URI}/ibl/v1/atoz/${letter}/programmes?page=${page}`);
+  const request = axios.get(`${BASE_URI}/ibl/v1/atoz/${letter.toLowerCase()}/programmes?page=${page}`);
   return {
     type: GET_PROGRAMMES,
     payload: request
--- src/components/a_to_z_menu.js
+++ src/components/a_to_z_menu.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import { getProgrammes } from '../actions';
+import { connect } from 'react-redux';
 
-export default (props) => {
-  return (
-    <div className="atoz-menu">
-    </div>
-  );
+require('../styles/a_to_z_menu.scss');
+
+class AToZMenu extends Component {
+  goToPage(letter) {
+    this.props.getProgrammes(letter,1);
+    browserHistory.push(`/a-z/${letter}`);
+  }
+
+  renderLinks() {
+    const options = [
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+      "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0-9"
+    ];
+    return options.map((option) => {
+      return (
+        <a className="menu-link" key={option} onClick={(event) => this.goToPage(option)}>{option}</a>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="atoz-menu row">
+        { this.renderLinks() }
+      </div>
+    );
+  }
 }
+
+export default connect(null, { getProgrammes })(AToZMenu);
