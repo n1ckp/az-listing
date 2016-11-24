e43187b5c663da9b7846ec5855a6b957ce18d0ac
Nick Phillips <nicktgphillips@gmail.com>
2016-11-24 09:48:29 +0000

    added tests for programmelistitem image size

--- test/components/programme_list_item.spec.js
+++ test/components/programme_list_item.spec.js
@@ -10,15 +10,16 @@ describe('(Component) ProgrammeListItem', function() {
 
   describe("given a valid programme as params", function() {
     let rootComponent;
+    let programmeData;
     beforeEach(function() {
-      let programmeData = {
+      programmeData = {
         id: "1",
         title: "Eastenders",
         images: {
           standard: "http://ichef.bbci.co.uk/images/ic/{recipe}/p017mqg6.jpg"
         }
       };
-      rootComponent = renderComponent(ProgrammeListItem, programmeData);
+      rootComponent = renderComponent(ProgrammeListItem, { programme: programmeData, img_size: "large" });
     });
 
     it("displays the programme title", function() {
@@ -26,7 +27,17 @@ describe('(Component) ProgrammeListItem', function() {
     });
 
     it("displays the programme image", function() {
-      expect(rootComponent.find('img').length).to.eql(1);
+      expect(rootComponent.find('img').src).to.eql("http://ichef.bbci.co.uk/images/ic/560x315/p017mqg6.jpg");
+    });
+
+    it("displays an image with small size", function() {
+      rootComponent = renderComponent(ProgrammeListItem, { programme: programmeData, img_size: "small" });
+      expect(rootComponent.find('img').src).to.eql("http://ichef.bbci.co.uk/images/ic/192x108/p017mqg6.jpg");
+    });
+
+    it("displays an image with medium size", function() {
+      rootComponent = renderComponent(ProgrammeListItem, { programme: programmeData, img_size: "medium" });
+      expect(rootComponent.find('img').src).to.eql("http://ichef.bbci.co.uk/images/ic/406x228/p017mqg6.jpg");
     });
 
   });
