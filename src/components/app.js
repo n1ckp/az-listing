7579dc300a5d78e5bb33f7ed1577a160fd256058
Nick Phillips <nicktgphillips@gmail.com>
2016-11-24 20:12:05 +0000

    app now rendering atoz menu

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+import AToZMenu from './a_to_z_menu';
+
 require('../styles/app.scss');
 
 class App extends Component {
   render() {
     return (
       <div className="main-container">
+        <AToZMenu />
         {this.props.children}
       </div>
     );
