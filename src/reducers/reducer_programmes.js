cebca5c523887520592d30bb90dea412d826c94e
Nick Phillips <nicktgphillips@gmail.com>
2016-11-24 16:24:39 +0000

    programme reducer setting boolean for more pages

--- src/reducers/reducer_programmes.js
+++ src/reducers/reducer_programmes.js
@@ -5,7 +5,9 @@ const INITIAL_STATE = {}
 export default function(state=INITIAL_STATE, action) {
   switch(action.type) {
     case GET_PROGRAMMES:
-      return {...state, programmes: action.payload.data.atoz_programmes.elements}
+      const { count, page, per_page } = action.payload.data.atoz_programmes;
+      const more_pages = (count > page*per_page);
+      return {...state, programmes: action.payload.data.atoz_programmes.elements, more_pages};
     default:
       return state;
   }
