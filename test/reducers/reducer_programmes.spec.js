56648c1843a9deb0befd98e0b17dd794dea2fffb
Nick Phillips <nicktgphillips@gmail.com>
2016-11-24 16:20:09 +0000

    added test for reducer to set boolean for more pages

--- test/reducers/reducer_programmes.spec.js
+++ test/reducers/reducer_programmes.spec.js
@@ -13,17 +13,35 @@ describe("(Reducer) ProgrammeReducer", function() {
   });
 
   describe("GET_PROGRAMMES",function() {
-
-    it("sets the global state for programmes", function() {
-      let initialState = {};
-      let action = {
+    let action;
+    beforeEach(function() {
+      action = {
         type: GET_PROGRAMMES,
         payload: { data: { atoz_programmes: {
+          count: 2,
+          page: 1,
+          per_page: 20,
           elements: [
             "Eastenders", "Another Programme"
           ]
         }}}};
-      expect(reducer(initialState, action)).to.eql({programmes:["Eastenders", "Another Programme"]});
+    });
+
+    it("sets the global state for programmes", function() {
+      const initialState = {};
+      expect(reducer(initialState, action).programmes).to.eql(["Eastenders", "Another Programme"]);
+    });
+
+    it("sets the global state for 'more_pages' to false if there aren't any more pages", function() {
+      const initialState = {};
+      expect(reducer(initialState, action).more_pages).to.eql(false);
+    });
+
+    it("sets the global state for 'more_pages' to true if there are more programmes", function() {
+      const initialState = {};
+      action.payload.data.atoz_programmes.count = 4;
+      action.payload.data.atoz_programmes.per_page = 2;
+      expect(reducer(initialState, action).more_pages).to.eql(true);
     });
 
   });
